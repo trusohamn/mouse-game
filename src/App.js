e70acacb456932de907b0c1c4bf19cbfa29b7758
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-11-29 22:16:56 +0100

    dragon changing direction

--- src/App.js
+++ src/App.js
@@ -2,34 +2,34 @@ import "./App.css";
 import { useState } from "react";
 import dragon from "./dragon.gif";
 
-const Element = ({ xy }) => {
-  const style = {
-    top: `${xy[1]}px`,
-    left: `${xy[0]}px`,
-  };
-
-  // TODO read it from properties of image
-  const styleImage = {
-    marginLeft: "-80px",
-    marginTop: "-120px",
+const Element = ({ xy, movement }) => {
+  const degree = 0;
+  const scale = movement[0] > 0 ? -1 : 1;
+  const stylePosition = {
+    top: `${xy[1] - 140}px`,
+    left: `${xy[0] - 100}px`,
+    transform: `rotate(${degree}deg) scaleX(${scale})`,
   };
 
   return (
-    <div className="Element" style={style}>
-      <img src={dragon} alt={dragon} style={styleImage}></img>
+    <div className="Element" style={stylePosition}>
+      <img src={dragon} alt={dragon}></img>
     </div>
   );
 };
 
 function App() {
   const [xy, setXy] = useState([0, 0]);
+  const [movement, setMovement] = useState([0, 0]);
+
   const onMouseMove = (e) => {
+    setMovement([e.movementX, e.movementY]);
     setXy([e.clientX, e.clientY]);
   };
 
   return (
     <div className="App" onMouseMove={onMouseMove}>
-      <Element xy={xy}></Element>
+      <Element xy={xy} movement={movement}></Element>
     </div>
   );
 }
