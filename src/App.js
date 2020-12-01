b46597f2e9c6e04215c60004e34b1e1b2a4393b4
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-12-01 11:34:42 +0100

    works with many eggs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-import { useState, useMemo, useEffect } from "react";
+import { useState, useEffect } from "react";
 import dragon from "./dragon.gif";
 import dragonEgg from "./dragonEgg.png";
-import babyDragon from "./baby-dragon.jpg";
+import dragonBaby from "./baby-dragon.jpg";
 
-const Element = ({ xy, movement }) => {
+const iconWidth = 100;
+const iconHeight = 100;
+
+const Mouse = ({ xy, movement }) => {
   const degree = 0;
   const scale = movement[0] > 0 ? -1 : 1;
   const stylePosition = {
@@ -14,56 +17,77 @@ const Element = ({ xy, movement }) => {
   };
 
   return (
-    <div className="Element" style={stylePosition}>
+    <div className="Mouse" style={stylePosition}>
       <img src={dragon} alt={dragon}></img>
     </div>
   );
 };
 
-const Final = ({ position, found }) => {
+const Final = ({ position }) => {
   const stylePosition = {
-    top: `${position[1] - 50}px`,
-    left: `${position[0] - 50}px`,
+    top: `${position[1]}px`,
+    left: `${position[0]}px`,
   };
 
   return (
-    <div className="Element" style={stylePosition}>
+    <div className="Mouse" style={stylePosition}>
       <img
-        src={found ? babyDragon : dragonEgg}
+        src={dragonEgg}
         alt={dragonEgg}
-        width={100}
-        heigth={100}
+        width={iconWidth}
+        heigth={iconHeight}
       ></img>
     </div>
   );
 };
+
+const Found = ({ position }) => {
+  const stylePosition = {
+    top: `${position[1]}px`,
+    left: `${position[0]}px`,
+  };
+
+  return (
+    <div className="Mouse" style={stylePosition}>
+      <img
+        src={dragonBaby}
+        alt={dragonBaby}
+        width={iconWidth}
+        heigth={iconHeight}
+      ></img>
+    </div>
+  );
+};
+
 const App = () => {
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   const [xy, setXy] = useState([0, 0]);
   const [movement, setMovement] = useState([0, 0]);
-  const [found, setFound] = useState(false);
+  const [found, setFound] = useState([]);
   const [finalPosition, setFinalPosition] = useState(null);
+
+  const setupGoal = () =>
+    setFinalPosition([
+      Math.random() * width * 0.9 - iconWidth / 2,
+      Math.random() * height * 0.9 - iconHeight / 2,
+    ]);
 
   useEffect(() => {
     const { innerWidth, innerHeight } = window;
     setWidth(innerWidth);
     setHeight(innerHeight);
-    if (width && height) {
-      setFinalPosition([
-        Math.random() * width * 0.9,
-        Math.random() * height * 0.9,
-      ]);
-    }
+    if (width && height) setupGoal();
   }, [height, width]);
 
   useEffect(() => {
     if (
       !!finalPosition &&
-      Math.abs(xy[0] - finalPosition[0] < 20) &&
-      Math.abs(xy[1] - finalPosition[1]) < 20
+      Math.abs(xy[0] - finalPosition[0]) < iconWidth &&
+      Math.abs(xy[1] - finalPosition[1]) < iconHeight
     ) {
-      setFound(true);
+      setFound([...found, <Found position={[...finalPosition]}></Found>]);
+      setupGoal();
     }
   }, [xy, finalPosition]);
 
@@ -74,10 +98,11 @@ const App = () => {
 
   return (
     <div className="App" onMouseMove={onMouseMove}>
-      <Element xy={xy} movement={movement}></Element>
+      <Mouse xy={xy} movement={movement}></Mouse>
       {!!finalPosition && (
         <Final position={finalPosition} found={found}></Final>
       )}
+      {found}
     </div>
   );
 };
